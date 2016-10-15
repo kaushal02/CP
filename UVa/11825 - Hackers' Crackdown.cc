b8b78d0ddc66cc02222c88e1c55577d9ed26d15e
kaushal02 <kaushal0181@gmail.com>
2016-10-15 15:33:01 +0530

    Update 11825 - Hackers' Crackdown.cc

--- UVa/11825 - Hackers' Crackdown.cc
+++ UVa/11825 - Hackers' Crackdown.cc
@@ -2,7 +2,7 @@
 _bitmask
 Traversing all submasks of a mask
 
-O(n * 3 ^ n) per test case
+O(3 ^ n) per test case
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -33,34 +33,14 @@ const ll inf = 1e16, mod = 1e9 + 7;
 const double eps = 1e-16;
 const int N = 18;
 
-int n, a[N], dp[1<<N];
-inline int f(int mask) {
-    if(dp[mask] + 1) return dp[mask];
-    int &ans = dp[mask];
-    ans = 0;
-    
-    int mb = 0; while(!(mask>>mb&1)) mb++;
-    mask ^= 1<<mb;
-    if(!mask) return ans = !a[mb];
-    
-    for(int smask = mask; ; smask = (mask & (smask - 1))) {
-        int temp = a[mb];
-        rep(i,n) if(smask>>i&1) temp &= a[i];
-        ans = max(ans, !temp + f(mask ^ smask));
-        
-        if(!smask) break;
-    }
-    return ans;
-}
-
+int n, a[N], ones[N], dp[1<<N], dp1[1<<N];
 int main() {
-    int casenum = 0;
+    int mb;
+    rep(i,N) ones[i] = (1<<i+1) - 1;
     
-    cin >> n;
-    casenum++;
+    int casenum = 0;
+    cin >> n; casenum++;
     while(n) {
-        mem(dp, -1);
-        dp[0] = 0;
         rep(i,n) {
             a[i] = 1<<i;
             int m; cin >> m;
@@ -68,11 +48,24 @@ int main() {
                 int x; cin >> x;
                 a[i] |= 1<<x;
             }
-            a[i] ^= (1<<n) - 1;
         }
-        cout << "Case " << casenum << ": " << f((1<<n)-1) << '\n';
         
-        cin >> n;
-        casenum++;
+        mb = 0;
+        mem(dp, 0);
+        mem(dp1, 0);
+        rep(i,1<<n) {
+            rep(j,n)if(i>>j&1) dp1[i] |= a[j];
+            dp1[i] = !(dp1[i] ^ ones[n-1]);
+            if(!i) continue;
+            int mask = 1<<mb ^ i;
+            for(int smask = mask; ; smask = (mask & (smask - 1))) {
+                dp[i] = max(dp[i], dp1[1<<mb ^ smask] + dp[mask ^ smask]);
+                if(!smask) break;
+            }
+            if(i == ones[mb]) mb++;
+        }
+        cout << "Case " << casenum << ": " << dp[ones[n-1]] << '\n';
+        
+        cin >> n; casenum++;
     }
 }
