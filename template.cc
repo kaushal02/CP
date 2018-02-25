587b3b95f74901267ec4f4df287ed0a9491afa5a
Kaushal Agrawal <kaushal0181@gmail.com>
2018-02-25 20:55:13 +0530

    :white_check_mark: my current template

--- template.cc
+++ template.cc
@@ -1,8 +1,7 @@
 // g++ -std=c++11 -O3 -fmax-errors=1
 #include <bits/stdc++.h>
 using namespace std;
-#define int ll
-typedef long long ll;
+#define int long long
 typedef long double dbl;
 typedef pair<int,int> pii;
 typedef vector<int> vi;
@@ -15,26 +14,25 @@ typedef vector<int> vi;
 #define mem(c,v) memset(c, v, sizeof c)
 #define uni(c) c.resize(distance(c.begin(), unique(all(c))))
 
-#define rep(i,n) for(ll i=0, _n=(n); i<_n; i++)
-#define rep1(i,n) for(ll i=1, _n=(n); i<=_n; i++)
+#define rep(i,n) for(int i=0, _n=(n); i<_n; i++)
+#define rep1(i,n) for(int i=1, _n=(n); i<=_n; i++)
 #define cout(d) cout << fixed << setprecision(d)
 #define err(x) cerr << #x << " = " << x << "\n"
 
 #define cases int _t_; cin >> _t_; rep1(_t,_t_)
 #define coutt cout << "Case " << _t << ": "
-#define io ios_base::sync_with_stdio (false); cin.tie(NULL);
+#define io ios_base::sync_with_stdio(false); cin.tie(NULL);
 
 const dbl eps=1e-12, pi=acosl(-1);
-const ll inf=1e16, mod=1e9+7;
-const int N=2e6+10;
+const int inf=1e16, mod=1e9+7, N=2e6+10;
 
 
 
 #undef int
 int main() {
-#define int ll
+#define int long long
 
-	
-	
+    
+
     return 0;
 }
